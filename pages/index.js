6a704520a728d8414dcd9252b236658692a60eab
Ria Kay <riakumar28@gmail.com>
2026-03-16 20:13:20 +0000

    fix indexdb

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,47 @@
+'use client';
+
 import Header from '../components/Header';
-import Image from 'next/image';
 import Footer from '../components/Footer';
 
 
+// import { openDatabase } from "@/lib/db";
+// const db = await openDatabase();
 export default function Home() {
   return (
     <div>
       <Header />
 
       <main>
-        {/* Welcome Section */}
         <section className="home-section intro-section">
           <div className="sectionBox introBox">
-          
+            <h1>Lorem Ipsum</h1>
+            <h3>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h3>
           </div>
         </section>
 
-        {/* misc Section */}
         <section className="home-section left">
-          <div className="sectionBox">
-            
-            </div>
+          <div className="sectionBox"></div>
 
-            <div className="section-text">
-              <h1>Store your comics</h1>
-              <h3>
-                At ComicHunt, we know that comic collecting is more than just a hobby—it's a passion. Our intuitive database allows you to easily log and manage your entire collection, while also contributing to a growing community of fellow collectors.
-              </h3>
-              <h3>
-                Whether you're tracking individual issues, organizing by series, or tagging storage locations like short and long boxes, ComicHunt is designed to reflect the way you collect in the real world. You can even flag missing issues or curate your own custom groups to keep everything in order.
-              </h3>
-            </div>
+          <div className="section-text">
+            <h1>Lorem Ipsum Section</h1>
+
+            <h3>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim
+              ad minim veniam.
+            </h3>
+
+            <h3>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+            </h3>
+          </div>
         </section>
-
-          <div>
-                      <Footer />
-                    </div>
       </main>
+
+      <Footer />
     </div>
   );
 }
