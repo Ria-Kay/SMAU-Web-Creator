9d40d51d15a3ca7ab92df9728d5dff5bf0e59329
Ria Kay <riakumar28@gmail.com>
2026-03-16 22:34:31 +0000

    basic home pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>SMAU Web Creator</h1>
+    </header>
+  );
+}
---
+++ src/pages/Editor.jsx
@@ -0,0 +1,8 @@
+export default function Editor() {
+  return (
+    <main>
+      <h2>Home Page</h2>
+      <p>This is the editor page.</p>
+    </main>
+  );
+}
